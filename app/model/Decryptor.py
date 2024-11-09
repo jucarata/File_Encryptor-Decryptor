a75689e30ee99277573aa7380d969f311f44424c
JCamiloRamirezTabares <juancaram21@gmail.com>
2024-11-09 00:04:58 -0500

    feat: add base structure for Decryptor class - added base structure to support Decryptor methods

---
+++ app/model/Decryptor.py
@@ -0,0 +1,13 @@
+class Decryptor:
+
+    def __init__(self):
+        pass
+
+
+    def decrypt(file, password: str):
+
+        return
+    
+
+    def decryptByPath(file, password: str):
+        return
